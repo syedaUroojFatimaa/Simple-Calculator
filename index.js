e506e6a13c1adeb8a74c78d8eeb9763553a3f08b
syedaUroojFatimaa <162017553+syedaUroojFatimaa@users.noreply.github.com>
2024-03-29 15:41:18 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,28 @@
+//#! /usr/bin/env node
+import inquirer from "inquirer";
+console.log("\n\t Welcome To \'SyedaUroojFatima'\ - CLI Simple Calculator\n");
+let answer = await inquirer.prompt([
+    { message: "Enter First Number", type: "number", name: "firstNumber" },
+    { message: "Enter Second Number", type: "number", name: "secondNumber" },
+    {
+        message: "Select one operator to perform operations",
+        type: "list",
+        name: "operator",
+        choices: ["Addition", "Subtraction", "Multiplication", "Division"],
+    }
+]);
+if (answer.operator === "Addition") {
+    console.log(answer.firstNumber + answer.secondNumber);
+}
+else if (answer.operator === "Subtraction") {
+    console.log(answer.firstNumber - answer.secondNumber);
+}
+else if (answer.operator === "Multiplication") {
+    console.log(answer.firstNumber * answer.secondNumber);
+}
+else if (answer.operator === "Division") {
+    console.log(answer.firstNumber / answer.secondNumber);
+}
+else {
+    console.log("Invalid Input");
+}
